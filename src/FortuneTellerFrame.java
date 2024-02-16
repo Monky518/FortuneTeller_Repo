e4bde07e090e2db8ff11618292e8efaa70dbabf4
Monky518 <zoeyrizor@gmail.com>
2024-02-16 12:59:48 -0500

    Not showing the image I want to add

--- src/FortuneTellerFrame.java
+++ src/FortuneTellerFrame.java
@@ -1,4 +1,12 @@
-public class FortuneTellerFrame expands JFrame
+import javax.swing.*;
+
+public class FortuneTellerFrame extends JFrame
 {
-    
+    public FortuneTellerFrame()
+    {
+        setTitle("My Little JFrame");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
---
+++ src/FortuneTellerViewer.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FortuneTellerViewer
+{
+    public static void main(String[] args)
+    {
+        JFrame inheritFrame = new FortuneTellerFrame();
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new BorderLayout());
+
+        // TOP PANEL
+        // A JLabel with text “Fortune Teller” (or something similar!) and an ImageIcon
+        // Find an appropriate non-commercial Fortune Teller image for your ImageIcon
+            // The JLabel has a constructor that takes a String and the ImageIcon
+            // Figure out from the Swing API or the content in Canvas how to display the text either above or below the ImageIcon
+        // Select a font face that works with your image and set the size to a larger value (try 36 and 48)
+        // Note that you have to add the image file to your IntelliJ project directory
+
+        // panel.add(object, BorderLayout.NORTH);
+
+        panel.add(new JLabel("Fortune Teller"), BorderLayout.NORTH);
+        panel.add(new JLabel(new ImageIcon("f:/fortuneTellerImage.png", "Fortune teller viewing a glowing orb")), BorderLayout.NORTH);
+
+        // finally, set JFrame as panel
+        inheritFrame.add(panel);
+    }
+}
